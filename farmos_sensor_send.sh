f4d88beadc6403a3d9d7bff7d17994e572038eb5
Ricardo Leonardo <raleonardo@gmail.com>
2021-03-24 02:53:08 +0000

    fix: curl should be silent in farmos_sensor_send

--- farmos_sensor_send.sh
+++ farmos_sensor_send.sh
@@ -10,5 +10,5 @@ source /etc/pi-env-monitor/farmos_config.py
 URL=${FARMOS_URL}/farm/sensor/listener/${FARMOS_API_PUB_KEY}?private_key=${FARMOS_API_PRIV_KEY}
 #echo $URL
 
-curl -H "Content-Type: application/json" -X POST -d $MEAS $URL
+curl -s -H "Content-Type: application/json" -X POST -d $MEAS $URL
 
