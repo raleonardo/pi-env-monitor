3e43d454d968b2082ca06f77c0c99e8168e0fd46
Ricardo Leonardo <raleonardo@gmail.com>
2021-03-24 00:46:20 +0000

    feat: adds farmos_sensor_send (simplified) script

---
+++ farmos_sensor_send.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+MEAS=$(/home/raleonardo/pi-env-monitor/dht22_get_env.py | jq '.' -Mc )
+echo $MEAS
+
+# setting farmos api variables
+source /etc/pi-env-monitor/farmos_config.py
+
+URL=${FARMOS_URL}/farm/sensor/listener/${FARMOS_API_PUB_KEY}?private_key=${FARMOS_API_PRIV_KEY}
+#echo $URL
+
+curl -D- -H "Content-Type: application/json" -X POST -d $MEAS $URL
+
